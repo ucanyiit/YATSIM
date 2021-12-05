14d8f4f431512a905d5c4ca8d165f16008452609
Yiğitcan Uçan <ucanyiit@gmail.com>
2021-12-05 15:29:40 +0300

    Update visualizer with docs

--- visualizer.py
+++ visualizer.py
@@ -6,8 +6,6 @@ import pygame
 from yatsim.cell_element import Direction
 from yatsim.game_grid import GameGridWithTrains
 
-BLACK = (0, 0, 0)
-WHITE = (200, 200, 200)
 BLOCK_SIZE = 64
 WINDOW_HEIGHT = 640
 WINDOW_WIDTH = 640
@@ -17,6 +15,7 @@ CLOCK = None
 
 
 def start_visualizer(game_grid: GameGridWithTrains):
+    """Visualizes the game grid using an endless loop."""
     global SCREEN, CLOCK, GAME_GRID, WINDOW_HEIGHT, WINDOW_WIDTH
     pygame.init()
     GAME_GRID = game_grid
@@ -24,7 +23,6 @@ def start_visualizer(game_grid: GameGridWithTrains):
     WINDOW_WIDTH = GAME_GRID.width * BLOCK_SIZE
     SCREEN = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
     CLOCK = pygame.time.Clock()
-    SCREEN.fill(BLACK)
 
     while True:
         draw_grid()
@@ -39,6 +37,7 @@ def start_visualizer(game_grid: GameGridWithTrains):
 
 
 def load_image(category: str, category_type: int, orientation: Direction):
+    """Loads the image for the train/cell and rotates it"""
     path = f"assets/{category}s/type{category_type}.png"
     cell = pygame.image.load(path).convert_alpha()
     cell = pygame.transform.scale(cell, (BLOCK_SIZE, BLOCK_SIZE))
@@ -47,15 +46,18 @@ def load_image(category: str, category_type: int, orientation: Direction):
 
 
 def draw_grid():
+    """Updates and draws the given grid."""
     GAME_GRID.update_view()
     display = GAME_GRID.display()
 
+    # Draw the new cell elements
     for y in range(0, GAME_GRID.height):
         for x in range(0, GAME_GRID.width):
             cell_type, orientation = display[y][x]
             image = load_image("cell", cell_type, orientation)
             SCREEN.blit(image, (x * BLOCK_SIZE, y * BLOCK_SIZE))
 
+    # Draw trains on the grid
     for train in GAME_GRID.trains:
         wagoons = train.get_geometry()
         for (x, y, train_type, orientation) in wagoons:
