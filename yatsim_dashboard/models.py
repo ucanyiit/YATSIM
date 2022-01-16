ec489bb65b304f1f4651eb5b79994a08d62b0162
Deniz Koluaçık <koluacik@disroot.org>
2022-01-16 19:24:11 +0300

    feat: Cell

--- yatsim_dashboard/models.py
+++ yatsim_dashboard/models.py
@@ -42,3 +42,123 @@ class Room(models.Model):
 
     def __str__(self):
         return f"<Room: ({self.pk}) - {self.owner.username}/{self.room_name}>"
+
+
+# TODO: save()
+# TODO: __str__()
+class Cell(models.Model):
+
+    CELL_TYPES = [
+        ("0", "Blank"),
+        ("1", "Straight"),
+        ("2", "Curved"),
+        ("3", "Y Junction"),
+        ("4", "Y Junction Mirrored"),
+        ("5", "X Junction"),
+        ("6", "Cross Roads"),
+        ("7", "Cross Bridge"),
+        ("8", "Station"),
+    ]
+
+    class Direction(models.TextChoices):
+        NORTH = ("0", "NORTH")
+        EAST = ("1", "EAST")
+        SOUTH = ("2", "SOUTH")
+        WEST = ("3", "WEST")
+
+    class State(models.TextChoices):
+        CCW = ("0", "Counter Clockwise Routing")
+        STRAIGHT = ("1", "Straight Routing")
+        CW = ("2", "Clockwise Routing")
+
+    room_id = models.ForeignKey(
+        Room, on_delete=models.CASCADE, related_name="cells", related_query_name="cell"
+    )
+    x = models.PositiveSmallIntegerField(blank=False, null=False)
+    y = models.PositiveSmallIntegerField(blank=False, null=False)
+    type = models.CharField(max_length=1, choices=CELL_TYPES, blank=False, null=False)
+    direction = models.CharField(
+        max_length=1, choices=Direction.choices, blank=False, null=False
+    )
+
+    state = models.CharField(max_length=1, choices=State.choices, null=True)
+
+    class Meta:
+        unique_together = (("room_id", "x", "y"),)
+
+    def rotate(self, direction: Direction) -> None:
+        self.direction = direction
+        self.save()
+
+    def switch_state(self) -> None:
+        if self.type not in ["3", "4", "5"]:
+            raise Exception("Not stateful cell: {self.x}, {self.y}")
+
+        if self.type == "3":
+            if self.state == "1":
+                self.state = "2"
+            elif self.state == "2":
+                self.state = "1"
+
+        elif self.type == "4":
+            if self.state == "1":
+                self.state = "0"
+            elif self.state == "0":
+                self.state = "1"
+
+        elif self.type == "5":
+            if self.state == "0":
+                self.state = "1"
+            elif self.state == "1":
+                self.state = "2"
+            else:  # self.state == "2":
+                self.state = "0"
+        self.save()
+
+    def next_cell(self, entry: str) -> str:
+
+        if self.type == "0":
+            raise Exception(f"0 cell {self.x}, {self.y}, {entry}")
+
+        if self.type in ["1", "8"]:
+            opposite = str((int(self.direction) + 2) % 4)
+            if entry == self.direction:
+                return opposite
+            if entry == opposite:
+                return self.direction
+            raise Exception(f"1 cell {self.x}, {self.y}, {entry}")
+        if self.type == "2":
+            opposite = str((int(self.direction) + 1) % 4)
+            if entry == self.direction:
+                return opposite
+            if entry == opposite:
+                return self.direction
+            raise Exception(f"2 cell {self.x}, {self.y}, {entry}")
+
+        if self.type == "3":
+            if entry == str((int(self.direction) + 3) % 4):
+                raise Exception(f"3 cell {self.x}, {self.y}, {entry}")
+
+            if entry != self.direction:
+                return self.direction
+
+            return str((int(self.direction) + (2 if self.state == "1" else 1)) % 4)
+
+        if self.type == "4":
+            if entry == str((int(self.direction) + 1) % 4):
+                raise Exception(f"4 cell {self.x}, {self.y}, {entry}")
+
+            if entry != self.direction:
+                return self.direction
+
+            return str((int(self.direction) + (2 if self.state == "1" else 3)) % 4)
+
+        if self.type == "5":
+            if entry != self.direction:
+                return self.direction
+            return str((int(self.direction) + 1 + int(entry)) % 4)
+
+        if self.type in ["6", "7"]:
+            return str((int(self.direction) + 2) % 4)
+
+        raise Exception("Impossible control flow")
